b5e7f224f5954a602aba9c23303ebc3fb21b205d
Kavinda Weerakoon <kw@kwnetic.com>
2022-09-20 18:20:11 -0400

    navigation update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       colors: {
         "weather-primary": "#00668A",
-        "weather-secondary": "004E71",
+        "weather-secondary": "#004E71",
       },
     },
     fontFamily: {
