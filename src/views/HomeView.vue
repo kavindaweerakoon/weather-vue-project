8b6c5cd8433872016607c1cbd44673374df0a9bf
Kavinda Weerakoon <kw@kwnetic.com>
2022-09-21 14:46:03 -0400

    added error handling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,22 +9,29 @@
         class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow=[0px_1px_0_0_#004E71]"
       />
       <ul
-      v-if="searchQuery"
+        v-if="searchQuery"
         class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
       >
-        <li
-          
-          v-for="searchResult in openweathermapSearchResults"
-          :key="searchResult.id"
-          class="py-2 cursor-pointer"
-        >
-          {{
-            searchResult.name +
-            ", " +
-            (searchResult.state ? searchResult.state + ", " : "") +
-            regionNames.of(searchResult.country)
-          }}
-        </li>
+        <p v-if="searchError">
+          Sorry, something went wrong. Please try again later.
+        </p>
+        <p v-if="!serverError && openweathermapSearchResults.length === 0">
+          No results found.
+        </p>
+        <template v-else>
+          <li
+            v-for="searchResult in openweathermapSearchResults"
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"
+          >
+            {{
+              searchResult.name +
+              ", " +
+              (searchResult.state ? searchResult.state + ", " : "") +
+              regionNames.of(searchResult.country)
+            }}
+          </li>
+        </template>
       </ul>
     </div>
   </main>
@@ -37,6 +44,7 @@ const openweathermapAPIKey = "9ed557cdf7c9ae9ddaf9a3ec13532116";
 const searchQuery = ref("");
 const queryTimeout = ref(null);
 const openweathermapSearchResults = ref(null);
+const searchError = ref(null);
 
 const regionNames = new Intl.DisplayNames(["en"], { type: "region" });
 
@@ -44,12 +52,16 @@ const getSearchResults = () => {
   queryTimeout.value = setTimeout(async () => {
     clearTimeout(queryTimeout.value);
     if (searchQuery.value !== "") {
-      const result = await axios.get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${searchQuery.value}&limit=5&appid=${openweathermapAPIKey}`
-      );
-      openweathermapSearchResults.value = result.data;
-      console.log(openweathermapSearchResults.value);
-      return;
+      try {
+        const result = await axios.get(
+          `http://api.openweathermap.org/geo/1.0/direct?q=${searchQuery.value}&limit=5&appid=${openweathermapAPIKey}`
+        );
+        openweathermapSearchResults.value = result.data;
+        console.log(openweathermapSearchResults.value);
+        return;
+      } catch {
+        searchError.value = true;
+      }
     }
     openweathermapSearchResults = null;
   }, 300);
